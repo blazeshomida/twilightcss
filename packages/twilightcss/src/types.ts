09206e183c8812dab5ff1cdc6d81e900f9bd2590
Blaze Shomida <shomida.dev@gmail.com>
2024-04-07 02:36:01 -1000

    feat(shadcn): :sparkles: add types for shadcn theme

--- packages/twilightcss/src/types.ts
+++ packages/twilightcss/src/types.ts
@@ -19,22 +19,50 @@ export type PrimitiveConfig<TConfig extends BaseConfig> = {
   };
 };
 
-type Tokens<TConfig extends BaseConfig> = {
+type TWTokens<TConfig extends BaseConfig> = {
   textColor?: Token<TConfig>;
   backgroundColor?: Token<TConfig>;
   borderColor?: Token<TConfig>;
 };
 
-type SelectorTokens<TConfig extends BaseConfig> = {
+type ShadcnTokens<TConfig extends BaseConfig> = Partial<{
+  background: `${TConfig["color"]}-${TConfig["shade"]}`;
+  foreground: `${TConfig["color"]}-${TConfig["shade"]}`;
+  card: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "card-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  popover: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "popover-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  primary: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "primary-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  secondary: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "secondary-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  muted: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "muted-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  accent: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "accent-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  destructive: `${TConfig["color"]}-${TConfig["shade"]}`;
+  "destructive-foreground": `${TConfig["color"]}-${TConfig["shade"]}`;
+  border: `${TConfig["color"]}-${TConfig["shade"]}`;
+  input: `${TConfig["color"]}-${TConfig["shade"]}`;
+  ring: `${TConfig["color"]}-${TConfig["shade"]}`;
+}>;
+
+type SelectorTokens<TConfig extends BaseConfig, TToken = TWTokens<TConfig>> = {
   selectors: string | string[];
-  tokens: Tokens<TConfig>;
+  tokens: TToken;
 };
-type MediaTokens<TConfig extends BaseConfig> = {
+type MediaTokens<TConfig extends BaseConfig, TToken = TWTokens<TConfig>> = {
   media: MediaProp | MediaProp[];
-  tokens: Tokens<TConfig>;
+  tokens: TToken;
 };
 
-export type Theme<TConfig extends BaseConfig> =
+export type ShadcnTheme<TConfig extends BaseConfig> =
+  | SelectorTokens<TConfig, ShadcnTokens<TConfig>>
+  | MediaTokens<TConfig, ShadcnTokens<TConfig>>
+  | (SelectorTokens<TConfig, ShadcnTokens<TConfig>> &
+      MediaTokens<TConfig, ShadcnTokens<TConfig>>);
+
+export type TwTheme<TConfig extends BaseConfig> =
   | SelectorTokens<TConfig>
   | MediaTokens<TConfig>
   | (SelectorTokens<TConfig> & MediaTokens<TConfig>);
@@ -42,6 +70,16 @@ export type Theme<TConfig extends BaseConfig> =
 export type StyleObject = {
   [key: string]: string | number | StyleObject;
 };
+
+export type ThemeOptions<TConfig extends BaseConfig> =
+  | {
+      type?: "tailwindcss";
+      themes: TwTheme<TConfig>[];
+    }
+  | {
+      type?: "shadcn";
+      themes: ShadcnTheme<TConfig>[];
+    };
 
 // Type Constants
 type MEDIA_QUERIES =
