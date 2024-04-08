7a3c8f815952c8e5bee5e4bfda5c126ba79d661f
Blaze Shomida <shomida.dev@gmail.com>
2024-04-07 16:30:38 -1000

    test(createTwilight): :recycle: refactor test common variables

--- packages/twilightcss/src/tests/createTwilight.test.ts
+++ packages/twilightcss/src/tests/createTwilight.test.ts
@@ -5,6 +5,24 @@ import {
   defineTheme,
   createTwilight,
 } from "@/index";
+const DEFAULT_COLORS = {
+  black: {
+    DEFAULT: "oklch(var(--clr-black) / <alpha-value>)",
+  },
+  current: {
+    DEFAULT: "var(--clr-current) ",
+  },
+  inherit: {
+    DEFAULT: "var(--clr-inherit) ",
+  },
+  transparent: {
+    DEFAULT: "var(--clr-transparent)",
+  },
+  white: {
+    DEFAULT: "oklch(var(--clr-white) / <alpha-value>)",
+  },
+};
+
 type Config = {
   variant: "primary" | "secondary";
   color:
@@ -42,38 +60,38 @@ const primitives = definePrimitives<Config>({
   },
 });
 
-describe("createTwilight(no-type): with tailwindcss theme, no type passed should default to tailwindcss", () => {
-  const darkTheme = defineTheme<Config>({
-    selectors: [".dark", ":root"],
-    media: [["(prefers-color-scheme: dark)", ":root"]],
-    tokens: {
-      textColor: {
-        neutral: {
-          primary: "neutral-50",
-          secondary: "neutral-300",
-        },
-        red: {
-          primary: "red-500",
-        },
+const tailwindDarkTheme = defineTheme<Config>({
+  selectors: ":root",
+  media: [["(prefers-color-scheme: dark)", ":root"]],
+  tokens: {
+    textColor: {
+      neutral: {
+        primary: "neutral-50",
+        secondary: "neutral-300",
       },
-      backgroundColor: {
-        neutral: {
-          primary: "neutral-950",
-        },
-        red: {
-          primary: "red-500",
-        },
-      },
-      borderColor: {
-        neutral: {
-          primary: "neutral-700",
-        },
+      red: {
+        primary: "red-500",
       },
     },
-  });
+    backgroundColor: {
+      neutral: {
+        primary: "neutral-950",
+      },
+      red: {
+        primary: "red-500",
+      },
+    },
+    borderColor: {
+      neutral: {
+        primary: "neutral-700",
+      },
+    },
+  },
+});
 
+describe("createTwilight(no-type): with tailwindcss theme, no type passed should default to tailwindcss", () => {
   const { twilightColors, twilightExtends } = createTwilight(primitives, {
-    themes: [darkTheme],
+    themes: [tailwindDarkTheme],
   });
   it("twilightColors: maps primitives to the primitives css custom properties for tailwind", () => {
     expect(twilightColors).toStrictEqual({
@@ -85,6 +103,7 @@ describe("createTwilight(no-type): with tailwindcss theme, no type passed should
         "950": "oklch(var(--clr-neutral-950) / <alpha-value>)",
       },
       red: { "500": "oklch(var(--clr-red-500) / <alpha-value>)" },
+      ...DEFAULT_COLORS,
     });
   });
 
@@ -112,40 +131,40 @@ describe("createTwilight(no-type): with tailwindcss theme, no type passed should
   });
 });
 describe("createTwilight(tailwindcss): with tailwindcss theme, type tailwindcss passed", () => {
-  const darkTheme = defineTheme<Config>({
-    selectors: [":root"],
-    media: [["(prefers-color-scheme: dark)", ":root"]],
-    tokens: {
-      textColor: {
-        neutral: {
-          primary: "neutral-50",
-          secondary: "neutral-300",
-        },
-        red: {
-          primary: "red-500",
-        },
-      },
-      backgroundColor: {
-        neutral: {
-          primary: "neutral-950",
-        },
-        red: {
-          primary: "red-500",
-        },
-      },
-      borderColor: {
-        neutral: {
-          primary: "neutral-700",
-        },
-      },
-    },
-  });
-
-  const { twilightColors, twilightExtends } = createTwilight(primitives, {
-    type: "tailwindcss",
-    themes: [darkTheme],
-  });
   it("twilightColors: maps primitives to the primitives css custom properties for tailwind", () => {
+    const darkTheme = defineTheme<Config>({
+      selectors: ":root",
+      media: [["(prefers-color-scheme: dark)", ":root"]],
+      tokens: {
+        textColor: {
+          neutral: {
+            primary: "neutral-50",
+            secondary: "neutral-300",
+          },
+          red: {
+            primary: "red-500",
+          },
+        },
+        backgroundColor: {
+          neutral: {
+            primary: "neutral-950",
+          },
+          red: {
+            primary: "red-500",
+          },
+        },
+        borderColor: {
+          neutral: {
+            primary: "neutral-700",
+          },
+        },
+      },
+    });
+
+    const { twilightColors, twilightExtends } = createTwilight(primitives, {
+      type: "tailwindcss",
+      themes: [darkTheme],
+    });
     expect(twilightColors).toStrictEqual({
       neutral: {
         "50": "oklch(var(--clr-neutral-50) / <alpha-value>)",
@@ -155,10 +174,15 @@ describe("createTwilight(tailwindcss): with tailwindcss theme, type tailwindcss 
         "950": "oklch(var(--clr-neutral-950) / <alpha-value>)",
       },
       red: { "500": "oklch(var(--clr-red-500) / <alpha-value>)" },
+      ...DEFAULT_COLORS,
     });
   });
 
   it("twilightExtends: maps tokens to the tokens css custom properties for tailwind", () => {
+    const { twilightExtends } = createTwilight(primitives, {
+      type: "tailwindcss",
+      themes: [tailwindDarkTheme],
+    });
     expect(twilightExtends).toStrictEqual({
       textColor: {
         neutral: {
@@ -182,45 +206,70 @@ describe("createTwilight(tailwindcss): with tailwindcss theme, type tailwindcss 
   });
 });
 
+const shadcnDark = defineShadcnTheme<Config>({
+  selectors: [".dark"],
+  media: ["(prefers-color-scheme: dark)", ":root"],
+  tokens: {
+    background: "neutral-950",
+    foreground: "neutral-50",
+    primary: "red-100",
+    "primary-foreground": "red-600",
+    secondary: "blue-100",
+    "secondary-foreground": "blue-600",
+    accent: "green-100",
+    "accent-foreground": "green-600",
+  },
+});
+
+const shadcnLight = defineShadcnTheme<Config>({
+  selectors: [":root", ".light"],
+  media: ["(prefers-color-scheme: light)", ":root"],
+  tokens: {
+    background: "neutral-50",
+    foreground: "neutral-950",
+    primary: "red-600",
+    "primary-foreground": "red-50",
+    secondary: "blue-600",
+    "secondary-foreground": "blue-50",
+    accent: "green-600",
+    "accent-foreground": "green-50",
+  },
+});
+
+const shadcnInvalidDark = defineShadcnTheme<Config>({
+  selectors: [".dark"],
+  media: ["(prefers-color-scheme: dark)", ":root"],
+  tokens: {
+    "": "neutral-950",
+    foreground: "neutral-50",
+    primary: "red-100",
+    "-foreground": "red-600",
+    secondary: "blue-100",
+    "secondary-foreground": "blue-600",
+    accent: "green-100",
+    "accent-foreground": "green",
+  },
+});
+
+const shadcnInvalidLight = defineShadcnTheme<Config>({
+  selectors: [":root", ".light"],
+  media: ["(prefers-color-scheme: light)", ":root"],
+  tokens: {
+    "": "neutral-50",
+    foreground: "neutral-950",
+    "-foreground": "blue-300",
+    secondary: "blue-600",
+    "secondary-foreground": "blue-50",
+    accent: "green-600",
+    "accent-foreground": "green-50",
+  },
+});
 describe("createTwilight(shadcn): with tailwindcss theme, type shadcn passed", () => {
-  const dark = defineShadcnTheme<Config>({
-    selectors: [".dark"],
-    media: ["(prefers-color-scheme: dark)", ":root"],
-    tokens: {
-      background: "neutral-950",
-      foreground: "neutral-50",
-      primary: "red-100",
-      "primary-foreground": "red-600",
-      secondary: "blue-100",
-      "secondary-foreground": "blue-600",
-      accent: "green-100",
-      "accent-foreground": "green-600",
-    },
-  });
-
-  const light = defineShadcnTheme<Config>({
-    selectors: [":root", ".light"],
-    media: ["(prefers-color-scheme: light)", ":root"],
-    tokens: {
-      background: "neutral-50",
-      foreground: "neutral-950",
-      primary: "red-600",
-      "primary-foreground": "red-50",
-      secondary: "blue-600",
-      "secondary-foreground": "blue-50",
-      accent: "green-600",
-      "accent-foreground": "green-50",
-    },
-  });
-  const { twilightColors, twilightExtends } = createTwilight<Config>(
-    primitives,
-    {
-      type: "shadcn",
-      themes: [light, dark],
-    }
-  );
-
   it("twilightColors: maps primitives to the primitives css custom properties for tailwind", () => {
+    const { twilightColors } = createTwilight<Config>(primitives, {
+      type: "shadcn",
+      themes: [shadcnLight, shadcnDark],
+    });
     expect(twilightColors).toStrictEqual({
       neutral: {
         "50": "oklch(var(--clr-neutral-50) / <alpha-value>)",
@@ -230,10 +279,15 @@ describe("createTwilight(shadcn): with tailwindcss theme, type shadcn passed", (
         "950": "oklch(var(--clr-neutral-950) / <alpha-value>)",
       },
       red: { "500": "oklch(var(--clr-red-500) / <alpha-value>)" },
+      ...DEFAULT_COLORS,
     });
   });
 
   it("twilightExtends: maps tokens to the tokens css custom properties for tailwind", () => {
+    const { twilightExtends } = createTwilight<Config>(primitives, {
+      type: "shadcn",
+      themes: [shadcnLight, shadcnDark],
+    });
     expect(twilightExtends).toStrictEqual({
       colors: {
         background: {
@@ -245,6 +299,31 @@ describe("createTwilight(shadcn): with tailwindcss theme, type shadcn passed", (
         primary: {
           DEFAULT: "oklch(var(--primary) / <alpha-value>)",
           foreground: "oklch(var(--primary-foreground) / <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "oklch(var(--secondary) / <alpha-value>)",
+          foreground: "oklch(var(--secondary-foreground) / <alpha-value>)",
+        },
+        accent: {
+          DEFAULT: "oklch(var(--accent) / <alpha-value>)",
+          foreground: "oklch(var(--accent-foreground) / <alpha-value>)",
+        },
+      },
+    });
+  });
+
+  it("should filter out invalid values", () => {
+    const { twilightExtends } = createTwilight<Config>(primitives, {
+      type: "shadcn",
+      themes: [shadcnInvalidLight, shadcnInvalidDark],
+    });
+    expect(twilightExtends).toStrictEqual({
+      colors: {
+        foreground: {
+          DEFAULT: "oklch(var(--foreground) / <alpha-value>)",
+        },
+        primary: {
+          DEFAULT: "oklch(var(--primary) / <alpha-value>)",
         },
         secondary: {
           DEFAULT: "oklch(var(--secondary) / <alpha-value>)",
