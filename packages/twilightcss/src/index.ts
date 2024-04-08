9ad304338ba356793a3bf63314fbe2407577f945
Blaze Shomida <shomida.dev@gmail.com>
2024-04-07 16:24:58 -1000

    feat(themes): :sparkles: add tailwind primitives to exports for easy theming

--- packages/twilightcss/src/index.ts
+++ packages/twilightcss/src/index.ts
@@ -3,4 +3,5 @@ export {
   defineTheme,
   defineShadcnTheme,
 } from "@/lib/config-definers";
+export { TAILWIND_PRIMITIVES as tailwindPrimitives } from "@/themes/tailwind";
 export { createTwilight } from "@/lib/createTwilight";
