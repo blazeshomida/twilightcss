72ed279d33892b2a1e849856b13cb60b4c2b8a24
Blaze Shomida <shomida.dev@gmail.com>
2024-04-11 01:19:54 -1000

    feat(config-definers): :fire: remove config definers in favor of exporting config types

--- packages/twilightcss/src/index.ts
+++ packages/twilightcss/src/index.ts
@@ -1,7 +1,7 @@
-export {
-  definePrimitives,
-  defineTheme,
-  defineShadcnTheme,
-} from "@/lib/config-definers";
+export type {
+  PrimitiveConfig,
+  TwTheme,
+  ShadcnTheme,
+} from "@/types/config-types";
 export { TAILWIND_PRIMITIVES as tailwindPrimitives } from "@/themes/tailwind";
 export { createTwilight } from "@/lib/createTwilight";
