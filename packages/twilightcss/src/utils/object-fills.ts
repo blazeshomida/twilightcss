a90a103c0097f952bb7210ec4e6be496678b9eb4
Blaze Shomida <shomida.dev@gmail.com>
2024-04-16 13:14:34 -1000

    feat(object-fills): :sparkles: add object fill for objectValues

--- packages/twilightcss/src/utils/object-fills.ts
+++ packages/twilightcss/src/utils/object-fills.ts
@@ -4,6 +4,10 @@ export function objectEntries<TObj extends object>(obj: TObj) {
   return Object.entries(obj) as [keyof TObj, TObj[keyof TObj]][];
 }
 
-export function objectKeys<T extends AnyObject>(object: T) {
-  return Object.keys(object) as (keyof T)[];
+export function objectKeys<TObj extends AnyObject>(object: TObj) {
+  return Object.keys(object) as (keyof TObj)[];
+}
+
+export function objectValues<TObj extends AnyObject>(obj: TObj) {
+  return Object.values(obj) as TObj[keyof TObj][];
 }
