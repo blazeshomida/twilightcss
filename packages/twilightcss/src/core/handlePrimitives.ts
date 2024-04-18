a725d7a114be9adfc1cee14110fbca3256e6718a
Blaze Shomida <shomida.dev@gmail.com>
2024-04-17 19:20:12 -1000

    feat(handlePrimitives): :sparkles: modify handlePrimitives for nested objects
    - implement generatePaths

--- packages/twilightcss/src/core/handlePrimitives.ts
+++ packages/twilightcss/src/core/handlePrimitives.ts
@@ -1,43 +1,36 @@
+import { handlePrefix, roundTo } from "@/utils/format-utils";
+import { DEFAULT_PRIMITIVES, DEFAULT_TOKENS } from "./constants";
+import { NestedObject } from "@/types/type-utils";
+import { generatePaths } from "@/utils/generatePaths";
 import { oklch } from "culori";
-import { BaseConfig, PrimitiveConfig } from "@/types/config-types";
-import { objectEntries } from "@/utils/object-fills";
-import { objectToCss } from "@/utils/objectToCss";
-import { DEFAULT_CSS_VARIABLES, DEFAULT_PRIMITIVES } from "./constants";
-function cleanParam(value: number, precision = 2): number {
-  return (
-    Math.round(parseFloat((value * 10 ** precision).toFixed(precision))) /
-    10 ** precision
-  );
-}
 
-export function handlePrimitives<TConfig extends BaseConfig>(
-  primitives: PrimitiveConfig<TConfig>
+export function handlePrimitives(
+  primitives: NestedObject<string>,
+  prefix: string
 ) {
-  const cssProperties: Record<string, string> = DEFAULT_CSS_VARIABLES;
-  const twPresetPrimitives: Record<
-    string,
-    Record<string, string>
-  > = DEFAULT_PRIMITIVES;
-  objectEntries(primitives).forEach(([color, shades]) => {
-    if (!shades) return;
-    const currentColor: Record<string, string> = {};
-    objectEntries(shades).forEach(([shadeProp, cssString]) => {
-      if (!cssString) return;
-      const userColor = oklch(cssString);
-      if (!userColor) return;
-      const { l = 0, c = 0, h = 0 } = userColor;
-      const shade = String(shadeProp);
-      const colorShade = shade === "DEFAULT" ? color : `${color}-${shade}`;
-      const property = `--clr-${colorShade}`;
-      cssProperties[property] =
-        `${Math.round(cleanParam(l) * 100)}% ${cleanParam(c)} ${cleanParam(h)}`;
-      currentColor[shade] = `oklch(var(${property}) / <alpha-value>)`;
-    });
-    twPresetPrimitives[color] = currentColor;
+  const { twPrimitivesPlugin, twilightPrimitives } = generatePaths(primitives, {
+    handlers: {
+      twPrimitivesPlugin: ({ key, value, map }) => {
+        const { l = 0, c = 0, h = 0 } = oklch(value) ?? {};
+        map.set(
+          `--${handlePrefix(prefix) + key}`,
+          `${Math.round(roundTo(l) * 100)}% ${roundTo(c)} ${roundTo(h)}`
+        );
+      },
+      twilightPrimitives: ({ key, map }) => {
+        map.set(
+          key,
+          `oklch(var(--${handlePrefix(prefix) + key}) / <alpha-value>)`
+        );
+      },
+    },
   });
   return {
-    cssPrimitives: objectToCss({ ":root": cssProperties }),
-    twPluginPrimitives: { ":root": cssProperties },
-    twPresetPrimitives,
+    twPrimitivesPlugin: {
+      ":root": { ...DEFAULT_PRIMITIVES(prefix), ...twPrimitivesPlugin },
+    },
+    twilightPrimitives: {
+      colors: { ...DEFAULT_TOKENS, ...twilightPrimitives },
+    },
   };
 }
