75986ce345af63f5f9ab57dbdf546870de6634b9
Blaze Shomida <shomida.dev@gmail.com>
2024-04-17 17:31:53 -1000

    feat(type-utils): :sparkles: add type utils (Pathify)
    - moved AnyObject type

---
+++ packages/twilightcss/src/types/type-utils.ts
@@ -0,0 +1,65 @@
+export type AnyObject = Record<PropertyKey, any>;
+
+export type NestedObject<TValue, TKey extends PropertyKey = PropertyKey> = {
+  [Key in TKey]?: NestedObject<TValue, TKey> | TValue;
+};
+
+type _Filler = 0;
+type TupLength<TTuple extends unknown[]> = TTuple["length"] & number;
+
+type NumToTup<
+  TLength extends number,
+  TFiller = _Filler,
+  _TAccumulator extends unknown[] = [],
+> =
+  TupLength<_TAccumulator> extends TLength
+    ? _TAccumulator
+    : NumToTup<TLength, TFiller, [..._TAccumulator, TFiller]>;
+
+type Subtract<A extends number, B extends number> =
+  NumToTup<A> extends [...infer A1, _Filler]
+    ? NumToTup<B> extends [...infer B1, _Filler]
+      ? Subtract<TupLength<A1>, TupLength<B1>>
+      : A
+    : A;
+
+export type Join<
+  TFirst extends string,
+  TSecond extends string,
+  Delimiter extends string = "-",
+> = `${TFirst}${Delimiter}${TSecond}`;
+
+type HandleFinal<
+  TKey,
+  TFinal,
+  TDelimiter extends string,
+  TPath,
+> = TKey extends TFinal
+  ? TPath extends `${infer Pre}${TDelimiter}`
+    ? Pre
+    : TPath
+  : TPath;
+
+export type Pathify<
+  TObj,
+  Final extends string = "DEFAULT",
+  Delimiter extends string = "-",
+  Prefix extends string = "",
+  // Increase as needed however,
+  // WARNING: higher depth increases memory usage
+  Depth extends number = 5,
+> = Depth extends 0
+  ? never
+  : {
+      [K in keyof TObj]: `${Prefix}${K extends Final ? "" : K & string}` extends infer Path
+        ? TObj[K] extends AnyObject
+          ? Pathify<
+              TObj[K],
+              Final,
+              Delimiter,
+              `${Path & string}${Delimiter}`,
+              Subtract<Depth, 1>
+            >
+          : HandleFinal<K, Final, Delimiter, Path>
+        : never;
+    }[keyof TObj];
