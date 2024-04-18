21ef15eb3304195b2ef1caddaea9e507c93bf35e
Blaze Shomida <shomida.dev@gmail.com>
2024-04-17 18:44:50 -1000

    refactor(type-configs): :recycle: refactor type-configs for new version

--- packages/twilightcss/src/types/config-types.ts
+++ packages/twilightcss/src/types/config-types.ts
@@ -1,93 +1,32 @@
-import { MEDIA_QUERIES, NAMED_COLORS_TYPE } from "@/types/type-constants";
-import { VALID_CSS_COLOR_FN } from "./type-constants";
+import { COLOR_MEDIA_QUERIES, Tailwind } from "./type-constants";
+import { NestedObject, Pathify } from "./type-utils";
 
-export type BaseConfig = {
-  color: string;
-  shade: string;
-  variant: string;
-};
+export type MediaProp = [COLOR_MEDIA_QUERIES, string] | [string, string];
 
-export type MediaProp = [MEDIA_QUERIES, string];
+type TokenConfig<TPrimitives, TKey extends PropertyKey> = Partial<
+  Record<
+    TKey,
+    NestedObject<Pathify<TPrimitives>, TKey | "DEFAULT"> | Pathify<TPrimitives>
+  >
+>;
 
-export type PrimitiveConfig<TConfig extends BaseConfig> = {
-  [Color in TConfig["color"]]?: {
-    [Shade in TConfig["shade"] | "DEFAULT"]?:
-      | NAMED_COLORS_TYPE
-      | VALID_CSS_COLOR_FN
-      | `#${string}`;
-  };
-};
+export type Tokens<TPrimitives, TKey extends PropertyKey> = Partial<
+  Record<
+    Tailwind.ColorConfig,
+    TokenConfig<TPrimitives, TKey> | TokenConfig<TPrimitives, string>
+  >
+>;
 
-export type Token<TConfig extends BaseConfig> = {
-  [Color in TConfig["color"]]?: {
-    [Variant in TConfig["variant"] | "DEFAULT"]?:
-      | `${Color}-${TConfig["shade"]}`
-      | Color;
-  };
-};
-
-type TWTokens<TConfig extends BaseConfig> = {
-  textColor?: Token<TConfig>;
-  backgroundColor?: Token<TConfig>;
-  borderColor?: Token<TConfig>;
-};
-
-type ShadcnTokens<
-  TConfig extends BaseConfig,
-  TColorShade extends
-    | `${TConfig["color"]}-${TConfig["shade"]}`
-    | TConfig["color"] =
-    | `${TConfig["color"]}-${TConfig["shade"]}`
-    | TConfig["color"],
-> = Record<string, TColorShade> &
-  Partial<{
-    background: TColorShade;
-    foreground: TColorShade;
-    card: TColorShade;
-    "card-foreground": TColorShade;
-    popover: TColorShade;
-    "popover-foreground": TColorShade;
-    primary: TColorShade;
-    "primary-foreground": TColorShade;
-    secondary: TColorShade;
-    "secondary-foreground": TColorShade;
-    muted: TColorShade;
-    "muted-foreground": TColorShade;
-    accent: TColorShade;
-    "accent-foreground": TColorShade;
-    destructive: TColorShade;
-    "destructive-foreground": TColorShade;
-    border: TColorShade;
-    input: TColorShade;
-    ring: TColorShade;
-  }>;
-
-type SelectorTokens<TConfig extends BaseConfig, TToken = TWTokens<TConfig>> = {
+type SelectorTokens<TPrimitives, TKey extends PropertyKey> = {
   selectors: string | string[];
-  tokens: TToken;
+  tokens: Tokens<TPrimitives, TKey>;
 };
-type MediaTokens<TConfig extends BaseConfig, TToken = TWTokens<TConfig>> = {
+type MediaTokens<TPrimitives, TKey extends PropertyKey> = {
   media: MediaProp | MediaProp[];
-  tokens: TToken;
+  tokens: Tokens<TPrimitives, TKey>;
 };
 
-export type ShadcnTheme<TConfig extends BaseConfig> =
-  | SelectorTokens<TConfig, ShadcnTokens<TConfig>>
-  | MediaTokens<TConfig, ShadcnTokens<TConfig>>
-  | (SelectorTokens<TConfig, ShadcnTokens<TConfig>> &
-      MediaTokens<TConfig, ShadcnTokens<TConfig>>);
-
-export type TwTheme<TConfig extends BaseConfig> =
-  | SelectorTokens<TConfig>
-  | MediaTokens<TConfig>
-  | (SelectorTokens<TConfig> & MediaTokens<TConfig>);
-
-export type ThemeOptions<TConfig extends BaseConfig> =
-  | {
-      type?: "tailwindcss";
-      themes: TwTheme<TConfig>[];
-    }
-  | {
-      type?: "shadcn";
-      themes: ShadcnTheme<TConfig>[];
-    };
+export type Theme<TPrimitives, TKey extends PropertyKey = PropertyKey> =
+  | SelectorTokens<TPrimitives, TKey>
+  | MediaTokens<TPrimitives, TKey>
+  | (SelectorTokens<TPrimitives, TKey> & MediaTokens<TPrimitives, TKey>);
