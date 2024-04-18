96c816bf064905ad149f2c157b9159672dc85031
Blaze Shomida <shomida.dev@gmail.com>
2024-04-17 17:33:50 -1000

    feat(isRecord): :sparkles: add isRecord to object-fills.ts

--- packages/twilightcss/src/utils/object-fills.ts
+++ packages/twilightcss/src/utils/object-fills.ts
@@ -1,4 +1,4 @@
-import { AnyObject } from "@/types/type-constants";
+import { AnyObject } from "@/types/type-utils";
 
 export function objectEntries<TObj extends object>(obj: TObj) {
   return Object.entries(obj) as [keyof TObj, TObj[keyof TObj]][];
@@ -10,4 +10,13 @@ export function objectKeys<TObj extends AnyObject>(object: TObj) {
 
 export function objectValues<TObj extends AnyObject>(obj: TObj) {
   return Object.values(obj) as TObj[keyof TObj][];
+}
+
+export function isRecord(object: unknown): object is AnyObject {
+  return (
+    object !== null &&
+    object !== undefined &&
+    typeof object === "object" &&
+    !Array.isArray(object)
+  );
 }
